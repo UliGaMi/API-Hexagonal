8a732df6bbacad166d734bb89af377e7c995545d
UliGaMi <ulises.galvez@hotmail.com>
2023-05-11 21:09:03 -0600

    Primer commit

---
+++ movies/domain/Movie.js
@@ -0,0 +1,8 @@
+class Movie{
+    constructor(name, description, duration, ruta){
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.ruta = ruta;
+    }
+}
